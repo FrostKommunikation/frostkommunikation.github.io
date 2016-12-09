82c4d733db6360a32bdfb8128099cb70cc57b65f
Kristian Frost <frost.kristian@gmail.com>
2016-12-09 10:34:42 +0100

    removed ds store

--- frost-kommunikation-kommunikationsr-dgiver-tekstforfatter/assets/js/app.js
+++
@@ -1,6 +0,0 @@
-// JavaScript
-window.sr = ScrollReveal();
-sr.reveal('.services__wrap > div', { duration: 600, delay: 50 });
-sr.reveal('.cases__item', { duration: 600, delay: 50 });
-sr.reveal('.whatabout__wrap > div', { duration: 600, delay: 50 });
-sr.reveal('.footer', { duration: 600, delay: 50 });
